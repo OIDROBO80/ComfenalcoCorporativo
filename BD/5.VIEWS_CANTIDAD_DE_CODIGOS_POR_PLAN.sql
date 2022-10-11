a81c2a6aee540dc84d65b3db9ec684628f1615a5
Oscar Javier Idrobo Lopez <oidrobo@bancodebogota.com.co>
2022-10-11 09:39:28 -0500

    VIEW CANTIDAD DE CODIGOS POR PLAN

---
+++ BD/5.VIEWS_CANTIDAD_DE_CODIGOS_POR_PLAN.sql
@@ -0,0 +1,17 @@
+CREATE
+    ALGORITHM = UNDEFINED
+    DEFINER = `user_app`@`localhost`
+    SQL SECURITY DEFINER
+    VIEW `cantidad_codigos_por_plan` AS
+SELECT
+    UUID() AS `id`,
+    COUNT(0) AS `cantidadDeCodigos`,
+    `P`.`nombre` AS `nombrePlan`,
+    `EE`.`documento_numero` AS `documentoEmpresa`,
+    `CD`.`asignado` AS `asignado`
+FROM
+    (((`empresa_empleador_x_plan` `EEP`
+        JOIN `codigo_descuento` `CD` ON ((`EEP`.`id` = `CD`.`id_empresa_plan`)))
+        JOIN `empresa_empleador` `EE` ON ((`EEP`.`id_empresa_empleador` = `EE`.`id`)))
+        JOIN `planes` `P` ON ((`EEP`.`id_plan` = `P`.`id`)))
+GROUP BY `P`.`nombre` , `EE`.`id` , `CD`.`asignado`
