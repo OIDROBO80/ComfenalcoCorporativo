834b78cbaf6f01f7e6ad89e10b41bf081789e9bb
Oscar Javier Idrobo Lopez <oidrobo@bancodebogota.com.co>
2022-11-03 22:15:23 -0500

    new field to company

--- BD/5.VIEWS_CANTIDAD_DE_CODIGOS_POR_PLAN.sql
+++ BD/5.VIEWS_CANTIDAD_DE_CODIGOS_POR_PLAN.sql
@@ -5,13 +5,16 @@ CREATE
     VIEW `cantidad_codigos_por_plan` AS
 SELECT
     UUID() AS `id`,
-    COUNT(0) AS `cantidadDeCodigos`,
+    COUNT(`CD`.`asignado`) AS `cantidadDeCodigos`,
     `P`.`nombre` AS `nombrePlan`,
     `EE`.`documento_numero` AS `documentoEmpresa`,
-    `CD`.`asignado` AS `asignado`
+    (CASE
+         WHEN (`CD`.`asignado` IS NULL) THEN 0
+         ELSE `CD`.`asignado`
+        END) AS `asignado`
 FROM
     (((`empresa_empleador_x_plan` `EEP`
-        JOIN `codigo_descuento` `CD` ON ((`EEP`.`id` = `CD`.`id_empresa_plan`)))
+        LEFT JOIN `codigo_descuento` `CD` ON ((`EEP`.`id` = `CD`.`id_empresa_plan`)))
         JOIN `empresa_empleador` `EE` ON ((`EEP`.`id_empresa_empleador` = `EE`.`id`)))
         JOIN `planes` `P` ON ((`EEP`.`id_plan` = `P`.`id`)))
 GROUP BY `P`.`nombre` , `EE`.`id` , `CD`.`asignado`
